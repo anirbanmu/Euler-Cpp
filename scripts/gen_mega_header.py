36fe940765cf315fe8cf3f1e61cb8b6a33c126d8
Anirban Mukhopadhyay <anirbanmu@outlook.com>
2016-04-30 01:10:36 -0400

    Clean up mega header gen script.

--- scripts/gen_mega_header.py
+++ scripts/gen_mega_header.py
@@ -3,14 +3,19 @@
 
 import glob, os, sys
 
+header_cpp = '#ifndef _MEGA_PROB_H_\n#define _MEGA_PROB_H_\n\n'
+footer_cpp = '\n#endif'
+
+def include_header_cpp(header, rel_loc):
+    return '#include "' + os.path.relpath(header, os.path.split(rel_loc)[0]) + '"\n'
+
+def aggregate_includes_cpp(header_dir, rel_loc):
+    return ''.join([include_header_cpp(f, rel_loc) for f in sorted(glob.glob(os.path.join(header_dir, '*.h')))])
+
 header_loc = sys.argv[1]
 megaheader_loc = sys.argv[2]
 
-text = '#ifndef _MEGA_PROB_H_\n#define _MEGA_PROB_H_\n\n'
-for f in sorted(glob.glob(os.path.join(header_loc, '*.h'))):
-    text = text + '#include "' + os.path.relpath(f, os.path.split(megaheader_loc)[0]) + '"\n'
-
-text = text + '\n#endif'
+includes_cpp = aggregate_includes_cpp(header_loc, megaheader_loc)
 
 with open(megaheader_loc, 'w') as mega:
-    mega.write(text)
+    mega.write(header_cpp + includes_cpp + footer_cpp)

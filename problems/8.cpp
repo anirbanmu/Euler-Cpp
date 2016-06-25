b3fd319b52098703c3712ec247f9884c4f05339c
Anirban Mukhopadhyay <anirbanmu@outlook.com>
2016-06-24 22:11:09 -0400

    Make sure each digit is null terminated

--- problems/8.cpp
+++ problems/8.cpp
@@ -15,18 +15,28 @@ unsigned long find_product(const vector<unsigned>& numbers, unsigned long accumu
     return find_product(numbers, accumulated * numbers[begin_index], begin_index + 1, end_index);
 }
 
-unsigned long find_max_product(string number, unsigned consecutive_digits)
+vector<unsigned> extract_digits(string number)
 {
-    vector<unsigned> numbers;
+    vector<unsigned> digits;
+
+    auto scratch = string("0");
     for (const auto digit : number)
     {
-        numbers.emplace_back(strtoul(&digit, nullptr, 10));
+        scratch[0] = digit;
+        digits.emplace_back(stoi(scratch));
     }
 
+    return digits;
+}
+
+unsigned long find_max_product(string number, unsigned consecutive_digits)
+{
+    const vector<unsigned> digits = extract_digits(number);
+
     unsigned long max_product = 0;
-    for (size_t i = 0; i + consecutive_digits <= numbers.size(); ++i)
+    for (size_t i = 0; i + consecutive_digits <= digits.size(); ++i)
     {
-        max_product = max(max_product, find_product(numbers, 1, i, i + consecutive_digits));
+        max_product = max(max_product, find_product(digits, 1, i, i + consecutive_digits));
     }
 
     return max_product;
